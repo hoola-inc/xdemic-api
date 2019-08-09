df71a222e29a22e9adabeed8e41e68dd3279c10f
Ameer <muhammadameerhmza@gmail.com>
2019-08-09 12:34:52 +0500

    some changes

--- src/utilities/error-handler.utility.js
+++ src/utilities/error-handler.utility.js
@@ -1,9 +1,9 @@
 module.exports = app => {
-    app.use((req, res, next) => {
-        // This middleware throws an error, so Express will go straight to
-        // the next error handler
-        setImmediate(() => { next(new Error('Woops! route not found...')); });
-    });
+    // app.use((req, res, next) => {
+    //     // This middleware throws an error, so Express will go straight to
+    //     // the next error handler
+    //     setImmediate(() => { next(new Error('Woops! route not found...')); });
+    // });
 
     app.use((error, req, res, next) => {
         // Any request to this server will get here, and will send an HTTP
