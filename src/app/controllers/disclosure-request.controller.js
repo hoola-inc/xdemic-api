26fc74a85c7a489c0860d27aa4297e5c5079c71a
Ameer <muhammadameerhmza@gmail.com>
2019-08-27 22:57:01 +0500

    notification testing

--- src/app/controllers/disclosure-request.controller.js
+++ src/app/controllers/disclosure-request.controller.js
@@ -29,42 +29,68 @@ exports.showQRCode = (req, res, next) => {
 }
 
 exports.varifyClaims = (req, res, next) => {
-    const jwt = req.body.access_token;
-    console.log(jwt);
-    credentials.createVerification(jwt)
-        .then(creds => {
-            console.log(creds.pushToken);
-            const push = transports.push.send(creds.pushToken, creds.boxPub)
 
-            credentials.authenticateDisclosureResponse({
-                sub: creds.did,
-                exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
-                claim:
-                {
-                    "name": "Rizwan",
-                    "id": "Single",
-                    "date of birth": "01/02/1990",
-                    "phone_number": "0333 3333333"
-                }
-            })
-                .then(attestation => {
-                    console.log('notification sent from server side');
-                    return push(attestation)  // *push* the notification to the user's mobile app.
-                })
-                .then(data => {
-                    console.log('sending response')
-                    return res.status(200).json({
-                        status: true,
-                        data: data,
-                        message: "Push notification sent and should be recieved any moment, Accept the push notification in the uPort mobile application"
-                    })
-                })
-                .catch(err => {
-                    console.log(err.message);
-                    next(err);
-                })
+    const jwt = req.body.access_token
+    credentials.authenticateDisclosureResponse(jwt).then(creds => {
+        // take this time to perform custom authorization steps... then,
+        // set up a push transport with the provided 
+        // push token and public encryption key (boxPub)
+        const push = transports.push.send(creds.pushToken, creds.boxPub)
+
+        credentials.createVerification({
+            sub: creds.did,
+            exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
+            claim: { 'Identity': { 'Last Seen': `${new Date()}` } }
+            // Note, the above is a complex (nested) claim. 
+            // Also supported are simple claims:  claim: {'Key' : 'Value'}
+        }).then(attestation => {
+            console.log(`Encoded JWT sent to user: ${attestation}`)
+            console.log(`Decodeded JWT sent to user: ${JSON.stringify(decodeJWT(attestation))}`)
+            return push(attestation)  // *push* the notification to the user's uPort mobile app.
+        }).then(res => {
+            console.log(res)
+            console.log('Push notification sent and should be recieved any moment...')
+            console.log('Accept the push notification in the uPort mobile application')
+            // ngrok.disconnect()
         })
-        .catch(err => {
-            next(err);
-        })
+    })
+    // const jwt = req.body.access_token;
+    // console.log(jwt);
+    // console.log(decodeJWT(jwt));
+    // credentials.createVerification(jwt)
+    //     .then(creds => {
+    //         console.log(creds.pushToken);
+    //         const push = transports.push.send(creds.pushToken, creds.boxPub)
+
+    //         credentials.authenticateDisclosureResponse({
+    //             sub: creds.did,
+    //             exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
+    //             claim:
+    //             {
+    //                 "name": "Rizwan",
+    //                 "id": "Single",
+    //                 "date of birth": "01/02/1990",
+    //                 "phone_number": "0333 3333333"
+    //             }
+    //         })
+    //             .then(attestation => {
+    //                 console.log('notification sent from server side');
+    //                 return push(attestation)  // *push* the notification to the user's mobile app.
+    //             })
+    //             .then(data => {
+    //                 console.log('sending response')
+    //                 return res.status(200).json({
+    //                     status: true,
+    //                     data: data,
+    //                     message: "Push notification sent and should be recieved any moment, Accept the push notification in the uPort mobile application"
+    //                 })
+    //             })
+    //             .catch(err => {
+    //                 console.log(err.message);
+    //                 next(err);
+    //             })
+    //     })
+    //     .catch(err => {
+    //         next(err);
+    //     })
 }
