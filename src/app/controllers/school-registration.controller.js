9728cfbf7216ac2b12200bf720f837f0366e72b9
Ameer <muhammadameerhmza@gmail.com>
2019-09-07 22:40:51 +0500

    some refinements

--- src/app/controllers/school-registration.controller.js
+++ src/app/controllers/school-registration.controller.js
@@ -1,4 +1,9 @@
 const SchoolSchema = require('../models/school-registration.model');
+const { Credentials } = require('uport-credentials');
+const contants = require('../../constants/main.constant');
+
+
+const credentials = new Credentials(contants.credentials);
 
 
 exports.createNewSchool = (req, res, next) => {
@@ -22,16 +27,21 @@ exports.getSchool = (req, res, next) => {
     SchoolSchema.find()
         .then(data => {
             if (data.length > 0) {
-                console.log('inside else');
-                return res.status(200).json({
-                    status: true,
-                    data: data
-                })
+                sendSchoolSchema(data)
+                    .then(signedJwt => {
+                        return res.status(200).send({
+                            status: true,
+                            data: signedJwt
+                        })
+                    })
+                    .catch(err => {
+                        next(err.message)
+                    })
             } else {
                 return res.status(200).json({
                     status: false,
                     message: 'no record found'
-                })
+                });
             }
         })
         .catch(err => {
@@ -40,31 +50,16 @@ exports.getSchool = (req, res, next) => {
 }
 
 function sendSchoolSchema(schoolData) {
-    const decodeJWT = require('did-jwt').decodeJWT;
-    const transports = require('uport-transports').transport;
-
-    const pushToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NkstUiJ9.eyJpYXQiOjE1Njc1ODM2MDIsImV4cCI6MTU5OTExOTYwMiwiYXVkIjoiZGlkOmV0aHI6MHhkNzQxYTZkZDI3MTE1MjFlODc5OGZiZTkyYzEyZmNiOWQyZjQzY2YxIiwidHlwZSI6Im5vdGlmaWNhdGlvbnMiLCJ2YWx1ZSI6ImFybjphd3M6c25zOnVzLXdlc3QtMjoxMTMxOTYyMTY1NTg6ZW5kcG9pbnQvR0NNL3VQb3J0Lzc0Njk2YTE4LTE2ODctMzBiYy1hYzI3LWY1M2ViMTE0OTZiMCIsImlzcyI6ImRpZDpldGhyOjB4YTA1NmZmYmZkNjQ0ZTQ4MmFkOGQ3MjJjNGJlNGM2NmFhMDUyYWQ1YSJ9.p4EJRCsDndG61gO32xHBLm8bJkWJ6tQGv5R4OYf2rfFs57W5MMqe9kEdopOZqqn95G0Sz8kN2izL2Xi01cGEngE';
-    const boxPub = 'enU9f7sZB7JsbcHLSfset6uZgVA44xI6y039RO5srk4='
-
-    credentials.createVerification({
-        sub: 'did:ethr:0xa056ffbfd644e482ad8d722c4be4c66aa052ad5a',
-        exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
-        claim: schoolData // sending school schema to demic mobile app
-        // Note, the above is a complex (nested) claim. 
-        // Also supported are simple claims:  claim: {'Key' : 'Value'}
-    }).then(attestation => {
-        const push = transports.push.send(pushToken, boxPub);
-        console.log(`Encoded JWT sent to user: ${attestation}`)
-        console.log(`Decodeded JWT sent to user: ${JSON.stringify(decodeJWT(attestation))}`)
-        return push(attestation)  // *push* the notification to the user's mobile app.
-    }).then(res => {
-        console.log(res)
-        console.log('Push notification sent and should be recieved any moment...')
-        console.log('Accept the push notification in the xdemic mobile application')
+    return new Promise((resolve, reject) => {
+        credentials.createVerification({
+            sub: 'did:ethr:0xa056ffbfd644e482ad8d722c4be4c66aa052ad5a',
+            exp: Math.floor(new Date().getTime() / 1000) + 30 * 24 * 60 * 60,
+            claim: schoolData
+        }).then(attestation => {
+            resolve(attestation);
+        })
+            .catch(err => {
+                reject(err.message);
+            });
     })
-        .catch(err => {
-            console.log('Error');
-            console.log(err);
-        });
-    // })
 }
--- src/constants/main.constant.js
+++ src/constants/main.constant.js
@@ -1,3 +1,9 @@
 module.exports.environment = {
     'BASE_URL': 'https://xdemic.herokuapp.com/'
 }
+
+module.exports.credentials = {
+    appName: 'Xdemic',
+    did: 'did:ethr:0xd741a6dd2711521e8798fbe92c12fcb9d2f43cf1',
+    privateKey: '8986bea04ec687c45be90c5a6e259dbf125291f3a8ede0b595442c39d3322875'
+}
