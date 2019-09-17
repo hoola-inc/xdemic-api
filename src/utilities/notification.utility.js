cc7ca87db8352ee16193be5c4b544ffd64c4a801
Ameer <muhammadameerhmza@gmail.com>
2019-09-17 15:12:24 +0500

    making it better

--- src/utilities/notification.utility.js
+++ src/utilities/notification.utility.js
@@ -4,15 +4,10 @@ exports.sendNotification = (data) => {
     const subscription = data;
     // res.status(201).json({});
     const payload = JSON.stringify({ title: 'test' });
-
+    console.log('printing subscription');
     console.log(subscription);
 
-    webpush.sendNotification(subscription, payload)
-    .then(data => {
-        console.log('inside then i am working');
-        console.log(data)
-    })
-    .catch(error => {
+    webpush.sendNotification(subscription, payload).catch(error => {
         console.error(error.stack);
     });
 }
