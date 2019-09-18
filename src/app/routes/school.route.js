6da9b8c33e105a0fc9b8feb9b0a68f38b5347f27
Ameer <muhammadameerhmza@gmail.com>
2019-09-18 14:39:11 +0500

    route change for school

--- src/app/routes/school.route.js
+++ src/app/routes/school.route.js
@@ -2,7 +2,7 @@ module.exports = app => {
     const schoolController = require('../controllers/school.controller');
 
     app.post('/school', schoolController.createSchool);
-    app.get('/school', schoolController.getSchool);
+    app.get('/schools', schoolController.getSchool);
     app.get('/schoolwithjwt', schoolController.getSchoolWithSignedJWT);
 
     app.get('/httpschool', schoolController.displaySchoolOnHttp);
