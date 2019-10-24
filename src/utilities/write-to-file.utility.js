5093437d98d67af93e78668c37fd0813c726421c
Ameer <muhammadameerhmza@gmail.com>
2019-10-24 18:39:12 +0500

    convert obj to JSON

--- src/utilities/write-to-file.utility.js
+++ src/utilities/write-to-file.utility.js
@@ -4,7 +4,7 @@ exports.writeToFile = (did, dirName, data) => {
     return new Promise((resolve, reject) => {
         const path = require('path').join(__dirname, `../../public/files/${dirName}/${did}.json`);
         console.log(path);
-        fs.writeFile(path, data, (err) => {
+        fs.writeFile(path, JSON.stringify(data), (err) => {
             if (err) {
                 console.log(err);
                 reject('an error occured while creating file');
