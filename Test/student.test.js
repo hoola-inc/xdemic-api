b449a930adfdb0a3b43998cbc66c01204d198f7f
Ameer <muhammadameerhmza@gmail.com>
2019-11-18 14:09:32 +0500

    sample testcase for student testing

---
+++ Test/student.test.js
@@ -0,0 +1,37 @@
+
+
+let mongoose = require("mongoose");
+let Student = require('../src/app/models/student.model');
+
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Stuents', () => {
+    beforeEach((done) => { //Before each test we empty the database
+        Student.remove({}, (err) => {
+            done();
+        });
+    });
+    /*
+      * Test the /GET route
+      */
+    describe('/GET student', () => {
+        it('it should GET all the books', (done) => {
+            chai.request(server)
+                .get('/book')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array');
+                    res.body.length.should.be.eql(0);
+                    done();
+                });
+        });
+    });
+
+});
