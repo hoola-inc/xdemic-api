d7d744bf052f41189360f8dbef079e8b011ad309
Ameer <muhammadameerhmza@gmail.com>
2019-12-31 15:42:41 +0500

    testing

--- src/app/routes/admin.route.js
+++ src/app/routes/admin.route.js
@@ -2,5 +2,5 @@ module.exports = app => {
     const adminController = require('../controllers/admin.controller');
 
     app.get('/adminQRCode', adminController.adminQRCode);
-    app.post('/callback', adminController.createAdmin);
+    // app.post('/callback', adminController.createAdmin);
 };
