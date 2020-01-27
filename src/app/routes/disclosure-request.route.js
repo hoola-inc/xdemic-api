442a6bd6a8fd7ea4e23b9c891b32120ec9568162
HamzaBinSarfraz <muhammadameerhmza@gmail.com>
2020-01-27 18:24:29 +0500

    add route for testing disclosure req

--- src/app/routes/disclosure-request.route.js
+++ src/app/routes/disclosure-request.route.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
     const disclosureController = require('../controllers/disclosure-request.controller');
     app.get('/qrcode', disclosureController.showQRCode);
-    app.post('/callback', disclosureController.verifyClaims);
+    app.post('/callback/test', disclosureController.verifyClaims);
 
     // app.post('/notificationsubscription', disclosureController.sendNotification);
     // app.post('/foo', disclosureController.updateFoo);
