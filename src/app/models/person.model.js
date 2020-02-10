e7dbb9ae29730d892aafb41976751c11ca74eb02
hamzabinsarfraz <muhammadameerhmza@gmail.com>
2020-02-10 15:57:44 +0500

    person schema

--- src/app/models/person.model.js
+++ src/app/models/person.model.js
@@ -1,46 +1,118 @@
 const mongoose = require('mongoose');
+/**
+ * Schema design according to CLR
+ * find more here https://www.imsglobal.org/sites/default/files/spec/clr/v1p0/InfoModel/clr_InfoModel.html#Data_Profile
+ */
 
 const PersonSchema = mongoose.Schema({
-    id: String,
-    type: String,
-    address: {
-        id: String,
+    type: {
         type: String,
-        addressCountry: String,
-        addressLocality: String,
-        addressRegion: String,
-        postalCode: String,
-        postOfficeBoxNumber: String,
-        streetAddress: String,
+        default: 'Person'
+    },
+    address: {
+        id: {
+            type: String
+        },
+        type: {
+            type: String,
+            default: 'Person'
+        },
+        addressCountry: {
+            type: String,
+            default: ''
+        },
+        addressLocality: {
+            type: String,
+            default: ''
+        },
+        addressRegion: {
+            type: String,
+            default: ''
+        },
+        postalCode: {
+            type: String,
+            default: ''
+        },
+        postOfficeBoxNumber: {
+            type: String,
+            default: ''
+        },
+        streetAddress: {
+            type: String,
+            default: ''
+        },
     },
     description: String,
     endorsements: {
         id: String,
-        type: String,
+        type: {
+            type: String
+        },
         claim: String,
-        issuedOn: String,
+        issuedOn: {
+            type: Date,
+            default: Date.now()
+        },
         issuer: String,
-        revocationReason: String,
-        revoked: String,
+        revocationReason: {
+            type: String,
+            default: ''
+        },
+        revoked: {
+            type: String,
+            default: ''
+        },
         verification: String,
     },
-    email: String,
-    image: String,
-    name: String,
-    publicKey: {
-        id: String,
+    email: {
         type: String,
+        required: [true, 'Why no email'],
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    name: {
+        type: String,
+        required: [true, 'Why no name'],
+    },
+    publicKey: {
+        id: {
+            type: String
+        },
+        type: {
+            type: String
+        },
         owner: String,
         publicKeyPem: String,
     },
     revocationList: String,
-    sourcedId: String,
-    studentId: String,
-    telephone: String,
-    url: String,
+    sourcedId: {
+        type: {
+            type: String
+        },
+        default: ''
+    },
+    studentId: {
+        type: {
+            type: String
+        },
+        default: '',
+    },
+    telephone: {
+        type: String,
+        unique: true,
+        required: [true, 'Why no telephone'],
+        index: true,
+    },
+    url: {
+        type: String
+    },
     verification: {
         id: String,
-        type: String,
+        type: {
+            type: String
+        },
         allowedOrigins: String,
         creator: String,
         startsWith: String,
@@ -75,13 +147,13 @@ class PersonModel {
 
 PersonSchema.pre("save", function (next) {
     const self = this;
-    mongoose.models["Person"].findOne({ mobile: this.mobile }, function (err, results) {
+    mongoose.models["Person"].findOne({ telephone: this.telephone }, function (err, results) {
         if (err) {
             next(err);
         } else if (results) {
-            console.log('mobile number must be unique');
-            self.invalidate("mobile", "mobile number must be unique");
-            next("mobile number must be unique");
+            console.log('telephone number must be unique');
+            self.invalidate("telephone", "telephone number must be unique");
+            next("telephone number must be unique");
         } else {
             next();
         }
